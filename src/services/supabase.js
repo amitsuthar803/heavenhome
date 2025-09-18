febafc06e104030367c30a23bc8f244c1dc963b6
amitsuthar803 <amitsuthar803@gmail.com>
2025-09-18 07:24:53 +0530

    updated

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://mzoqbqkdojrbkpfalqeq.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16b3FicWtkb2pyYmtwZmFscWVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczOTUxNDYsImV4cCI6MjAyMjk3MTE0Nn0.bjkQUMWJGyfwV_eIRyT29hAS8-LriecxmJfUYZX3ppc";
+export const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseKey = process.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
